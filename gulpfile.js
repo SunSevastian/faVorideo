8bfa63636f7d0f2813f56d7534983ad114217a5a
Aleksandr Sevastyanov <sunsevastian@gmail.com>
2016-07-07 08:53:50 +0300

    first push

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var lr = require('tiny-lr'), // Минивебсервер для livereload
+    gulp = require('gulp'), // Сообственно Gulp JS
+    jade = require('gulp-jade'), // Плагин для Jade
+    stylus = require('gulp-stylus'), // Плагин для Stylus
+    livereload = require('gulp-livereload'), // Livereload для Gulp
+    myth = require('gulp-myth'), // Плагин для Myth - http://www.myth.io/
+    csso = require('gulp-csso'), // Минификация CSS
+    imagemin = require('gulp-imagemin'), // Минификация изображений
+    uglify = require('gulp-uglify'), // Минификация JS
+    concat = require('gulp-concat'), // Склейка файлов
+    connect = require('connect'), // Webserver
+    rename = require("gulp-rename");// переименование
+    notify =require('gulp-notify'),
+    sass = require('gulp-sass'),
+    server = lr();
+
+gulp.task('sass', function(){
+    gulp.src('app/sass/*.sass')
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('default',function(){
+    gulp.src('app/css/*.css') 
+      .pipe(concat('css/con/bundle.css'))
+      .pipe(gulp.dest('app/'))
+      .pipe(csso())
+      .pipe(rename('css/min/bundle.min.css'))
+      .pipe(gulp.dest('app/'))
+      .pipe(notify(' - Чык-Чырык!!!'))
+
+});
+// Объединение CSS и их минификация
+
+gulp.task('watch', function(){
+    gulp.watch('app/css/*.css', ['default'])
+});
